bd20accea50f7e868fc643db9a4d5c1bb66445d9
Bruno Moreno <brunomorello7@gmail.com>
2023-01-15 19:02:09 +0000

    feat: Product Controller created for autocomplete feature

--- src/main/java/pt/bmo/list4u/api/shoppinglist/service/ProductService.java
+++ src/main/java/pt/bmo/list4u/api/shoppinglist/service/ProductService.java
@@ -3,13 +3,13 @@ package pt.bmo.list4u.api.shoppinglist.service;
 import org.springframework.stereotype.Service;
 import pt.bmo.list4u.api.shoppinglist.model.Product;
 
-import java.util.Map;
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public interface ProductService {
     public Optional<Product> getById(Long id);
-    public Optional<Product> getAll(Map<String, String> queryParams);
+    public List<Product> getAll();
     public Product create(Product product);
-    public Optional<Product> update(Product product);
+    public Optional<Product> update(Long id, Product product);
 }
