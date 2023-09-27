9850ede7fe4e32e448ed8466212ed6e29a04f72d
Bruno Moreno <brunomorello7@gmail.com>
2023-09-27 20:06:51 +0100

    fix: CORS error in PROD

--- src/main/java/pt/bmo/list4u/api/shoppinglist/config/CorsConfig.java
+++ src/main/java/pt/bmo/list4u/api/shoppinglist/config/CorsConfig.java
@@ -1,20 +1,22 @@
 package pt.bmo.list4u.api.shoppinglist.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.Arrays;
+@Configuration(proxyBeanMethods = false)
+public class CorsConfig {
 
-@Configuration
-@EnableWebMvc
-public class CorsConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOriginPatterns("https://*.herokuapp.com:[*]")
-                .allowedMethods("GET","POST","PATCH", "PUT", "DELETE", "OPTIONS", "HEAD");
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOriginPatterns("https://*.herokuapp.com:[*]")
+                        .allowedMethods("GET","POST","PATCH", "PUT", "DELETE", "OPTIONS", "HEAD");
+            }
+        };
     }
 }
