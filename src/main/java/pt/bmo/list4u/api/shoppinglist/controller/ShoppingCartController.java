ea89809b760c8cf560ed52d259da40ca62a6cb23
Bruno Moreno <brunomorello7@gmail.com>
2023-09-27 19:21:39 +0100

    fix: CORS error in PROD

--- src/main/java/pt/bmo/list4u/api/shoppinglist/controller/ShoppingCartController.java
+++ src/main/java/pt/bmo/list4u/api/shoppinglist/controller/ShoppingCartController.java
@@ -6,7 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 import pt.bmo.list4u.api.shoppinglist.model.ShoppingCart;
 import pt.bmo.list4u.api.shoppinglist.service.ShoppingCartReportsService;
@@ -17,7 +25,6 @@ import java.util.Map;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "https://list4u-front.herokuapp.com/")
 @RequestMapping("/api/shopping-carts")
 public class ShoppingCartController {
 
