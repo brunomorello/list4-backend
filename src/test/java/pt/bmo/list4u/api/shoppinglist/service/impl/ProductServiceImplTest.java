a98ca80250ab22fd2153f3dbdbaf5b9e30e71821
Bruno Moreno <brunomorello7@gmail.com>
2023-11-15 13:22:52 +0000

    Fixed Unit Tests for ProductService

--- src/test/java/pt/bmo/list4u/api/shoppinglist/service/impl/ProductServiceImplTest.java
+++ src/test/java/pt/bmo/list4u/api/shoppinglist/service/impl/ProductServiceImplTest.java
@@ -10,13 +10,18 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import pt.bmo.list4u.api.shoppinglist.entity.ProductEntity;
+import pt.bmo.list4u.api.shoppinglist.mapper.ProductMapper;
 import pt.bmo.list4u.api.shoppinglist.model.Product;
 import pt.bmo.list4u.api.shoppinglist.repository.ProductRepository;
 import pt.bmo.list4u.api.shoppinglist.utils.FakeValues;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ProductServiceImplTest {
@@ -32,7 +37,7 @@ class ProductServiceImplTest {
     @Mock
     private Page page;
     @Captor
-    private ArgumentCaptor<Product> productArgumentCaptor;
+    private ArgumentCaptor<ProductEntity> productArgumentCaptor;
 
     private Product create() {
         return new Product(FakeValues.FAKE_LONG, FakeValues.FAKE_STR);
@@ -40,52 +45,78 @@ class ProductServiceImplTest {
 
     @Test
     void when_get_by_id_then_return_product() {
-//        Product product = create();
-//        Mockito.when(repository.findById(Mockito.anyLong())).thenReturn(Optional.of(product));
-//
-//        Optional<Product> optionalProduct = service.getById(FakeValues.FAKE_LONG);
-//
-//        assertTrue(optionalProduct.isPresent());
+        final var product = create();
+        when(repository.findById(anyLong()))
+                .thenReturn(Mono.just(ProductMapper.INSTANCE.domainToEntity(product)));
+
+        var productMono = service.getById(FakeValues.FAKE_LONG);
+
+        StepVerifier.create(productMono)
+                .expectNextCount(1)
+                .assertNext(productFound -> {
+                    assertEquals(productFound.id(), product.id());
+                    assertEquals(productFound.name(), product.name());
+                });
     }
 
     @Test
     void when_get_by_inexistent_id_then_return_empty() {
-//        Mockito.when(repository.findById(Mockito.anyLong())).thenReturn(Optional.empty());
-//        Optional<Product> optionalProduct = service.getById(FakeValues.FAKE_LONG);
-//
-//        assertTrue(optionalProduct.isEmpty());
+        when(repository.findById(anyLong())).thenReturn(Mono.empty());
+
+        var productMono = service.getById(FakeValues.FAKE_LONG);
+
+        StepVerifier.create(productMono)
+                .expectNextCount(0)
+                .verifyComplete();
     }
 
     @Test
     void when_create_product_then_verify_repository_save() {
-//        Product product = create();
-//        Mockito.when(repository.save(Mockito.any())).thenReturn(product);
-//
-//        service.create(product);
-//        Mockito.verify(repository).save(productArgumentCaptor.capture());
-//
-//        assertEquals(product, productArgumentCaptor.getValue());
+        final var product = create();
+        when(repository.save(Mockito.any()))
+                .thenReturn(Mono.just(ProductMapper.INSTANCE.domainToEntity(product)));
+
+        var productMono = service.create(product);
+
+        verify(repository).save(productArgumentCaptor.capture());
+        ProductEntity productArgumentCaptorValue = productArgumentCaptor.getValue();
+
+        StepVerifier.create(productMono)
+                .assertNext(productCreated -> {
+                    assertEquals(productArgumentCaptorValue.getId(), productCreated.id());
+                    assertEquals(productArgumentCaptorValue.getName(), productCreated.name());
+                });
     }
 
     @Test
     void when_update_existent_product_then_verify_repository_and_return_it() {
-//        Product product = create();
-//        Mockito.when(repository.findById(Mockito.anyLong())).thenReturn(Optional.of(product));
-//        Mockito.when(repository.save(Mockito.any())).thenReturn(product);
-//
-//        Optional<Product> optionalProduct = service.update(FakeValues.FAKE_LONG, product);
-//        Mockito.verify(repository).save(productArgumentCaptor.capture());
-//
-//        assertEquals(optionalProduct.get(), productArgumentCaptor.getValue());
+        final var productMock = create();
+        final var product = Product.builder()
+                        .id(FakeValues.FAKE_LONG)
+                        .name("TEST")
+                        .build();
+
+        when(repository.findById(anyLong()))
+                .thenReturn(Mono.just(ProductMapper.INSTANCE.domainToEntity(productMock)));
+
+        var productMono = service.update(FakeValues.FAKE_LONG, product);
+
+        StepVerifier.create(productMono)
+                .expectNextCount(1)
+                .assertNext(productUpdated -> {
+                    assertEquals(product, productUpdated);
+                });
     }
 
     @Test
     void when_update_inexistent_product_then_return_empty() {
-//        Product product = create();
-//        Mockito.when(repository.findById(Mockito.anyLong())).thenReturn(Optional.empty());
-//
-//        Optional<Product> optionalProduct = service.update(FakeValues.FAKE_LONG, product);
-//
-//        assertTrue(optionalProduct.isEmpty());
+        final var product = create();
+        when(repository.findById(anyLong())).thenReturn(Mono.empty());
+
+        var productMono = service.update(FakeValues.FAKE_LONG, product);
+
+        StepVerifier.create(productMono)
+                .expectNextCount(0)
+                .verifyComplete();
     }
 }
